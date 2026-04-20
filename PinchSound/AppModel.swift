47275c5f83b17c9bf6c1031e8026a5bbcaad19e7
Irischan33 <zipingxu@usc.edu>
2026-04-20 16:30:17 -0700

    Initial Commit

---
+++ PinchSound/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  PinchSound
+//
+//  Created by 许紫萍 on 4/20/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ PinchSound/PinchSoundApp.swift
@@ -0,0 +1,33 @@
+//
+//  PinchSoundApp.swift
+//  PinchSound
+//
+//  Created by 许紫萍 on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct PinchSoundApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
